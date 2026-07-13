4e60d42437e67ec771eace0ddf841fe23dc3730c
odriozolanelson <odriozolanelson@hotmail.com>
2026-07-13 18:01:06 -0300

    Add files via upload

---
+++ soluciones.sql
@@ -0,0 +1,40 @@
+﻿-- ══════════════════════════════════════════
+-- RetailChain — UNION y UNION ALL
+-- Autor: Nelson Odriozola
+-- Fecha: 13/7/2026
+-- ══════════════════════════════════════════
+
+-- ── CONSULTA 1: UNION ────────────────────
+-- Reporte de Catálogo Unificado
+-- Pregunta de negocio: ¿Qué productos únicos comercializa
+-- la empresa en toda su red de sucursales?
+-- Operador: UNION (elimina filas completamente duplicadas)
+
+SELECT id_producto, nombre_producto, categoria FROM inventario_sucursal_norte
+UNION
+SELECT id_producto, nombre_producto, categoria FROM inventario_sucursal_sur;
+
+
+-- ── CONSULTA 2: UNION ALL ────────────────
+-- Auditoría de Stock Total
+-- Pregunta de negocio: ¿Cuántos registros físicos de stock
+-- existen en total entre ambas sucursales?
+-- Operador: UNION ALL (mantiene todos los registros incluyendo duplicados)
+
+SELECT * FROM inventario_sucursal_norte
+UNION ALL
+SELECT * FROM inventario_sucursal_sur;
+
+
+
+-- ── CONSULTA 3: COMPARACIÓN DE RESULTADOS ─
+-- Ejecutá estas dos consultas para comparar cuántas filas
+-- devuelve cada operador y explicá la diferencia en tu README
+
+SELECT COUNT(*) AS filas_union     FROM (SELECT id_producto, nombre_producto, categoria FROM inventario_sucursal_norte
+UNION
+SELECT id_producto, nombre_producto, categoria FROM inventario_sucursal_sur)     AS resultado_union;
+
+SELECT COUNT(*) AS filas_union_all FROM (SELECT * FROM inventario_sucursal_norte
+UNION ALL
+SELECT * FROM inventario_sucursal_sur) AS resultado_union_all;
